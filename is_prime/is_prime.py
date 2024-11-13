d58e43e8154e44874842e180e952f7070ea79c50
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 11:41:24 +0000

    Solved 'is prime' (.py)

---
+++ is_prime/is_prime.py
@@ -0,0 +1,77 @@
+# To remember 
+# % Remainder after division - the decimal part
+# // modulus of division - the closest int 
+
+
+from math import sqrt
+def is_prime(n):
+  if n < 2:
+    return False
+  for i in range(2, int(sqrt(n))+1):
+    if n % i == 0:
+      return False
+  return True
+      
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
