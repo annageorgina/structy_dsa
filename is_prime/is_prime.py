0bea2ca701263ecb4e59cd01a8d438adf174fca6
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 11:47:45 +0000

    Solved 'is prime' (.py)

--- is_prime/is_prime.py
+++ is_prime/is_prime.py
@@ -11,10 +11,24 @@ def is_prime(n):
     if n % i == 0:
       return False
   return True
-      
 
 
-    
+
+
+from math import sqrt
+def is_prime(num):
+  if num < 2:
+    return False
+  i = 2
+  while i < int((sqrt(num)+1)):
+    if num % i == 0:
+      return False
+    i += 1
+  return True
+
+
+#Time & Space: O(n) 
+# (actually O(sqrt(n)) but simplifies to O(n))
   
 
 
