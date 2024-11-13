a3d693092c33b38db00c8d4683346f318d938115
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 16:18:16 +0000

    Solved 'sum numbers recursive' (.py)

---
+++ sum_numbers_recursive/sum_numbers_recursive.py
@@ -0,0 +1,87 @@
+
+
+def sum_numbers_recursive(numbers):
+  if len(numbers) == 0:
+    return 0
+  return numbers[0] + sum_numbers_recursive(numbers[1:])
+
+
+#Time O(n^2) because n function calls and slicing time n 
+#Space O(n^2) because n function calls and subarray length n 
+
+
+
+
+def sum_numbers(numbers):
+  a = 0
+  for i in numbers:
+    a += i
+  return a
+
+
+# O(n) 'for' array loop
+
+
+
+
+def factorial(n):
+  if n == 0:
+    return 1
+  return n * factorial(n-1)
+
+
+print(factorial(7))
+
+
+
+
+def sum_numbers_recursive(nums):
+  if nums == []:
+    return 0
+  num = nums.pop()
+  return num + sum_numbers_recursive(nums)
+  
+print(sum_numbers_recursive([5, 2, 9, 10]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
