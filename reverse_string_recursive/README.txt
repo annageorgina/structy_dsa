ba580178df904811ced86d000a146c3cb18381d4
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 16:31:24 +0000

    Solved 'reverse string recursive' (.py)

---
+++ reverse_string_recursive/README.txt
@@ -0,0 +1,19 @@
+Write a function, reverse_string, that takes in a string as an argument. The function should return the string with its characters in reverse order. You must do this recursively.
+
+test_00
+
+reverse_string("hello") # -> "olleh"
+
+test_01
+
+reverse_string("abcdefg") # -> "gfedcba"
+
+
+
+test_02
+
+reverse_string("stopwatch") # -> "hctawpots"
+
+test_03
+
+reverse_string("") # -> ""
---
+++ reverse_string_recursive/reverse_string_recursive.py
@@ -0,0 +1,102 @@
+#non recursive = swap the characters
+def reverse_string_brute(s):
+  lis_s = list(s)
+  i = 0
+  j = len(lis_s)-1
+  while j > i:
+    lis_s[i], lis_s[j] = lis_s[j], lis_s[i]
+    i += 1
+    j -= 1
+  return ''.join(lis_s)
+
+
+#recursive
+def reverse_string_one(s):
+  s = list(s)
+  if len(s) == 0:
+    return ''
+  str_of_list = ''.join((s[:-1]))
+  return s[-1] + reverse_string(str_of_list)
+
+
+#recursive
+def reverse_string(s):
+  if len(s) == 0:
+    return ''
+  return reverse_string(s[1:]) + s[0]
+  #return s[-1] + reverse_string(s[:-1]) CAN BE EITHER OR !!! 
+
+
+# You can either add the last char to the fron tor the first char to the end
+
+
+# Time O(n^2): n function calls & n slices made
+# Space O(n^2):  n function calls and n substrings made
+
+
+  
+print(reverse_string("abcdefg"))
+
+
+def reverse_string(str):
+  i = 0
+  j = len(str) - 1 
+  str = list(str)
+  while j >= i:
+    str[i], str[j] = str[j], str[i]
+    i += 1
+    j -= 1
+  return ''.join(str)
+
+
+print(reverse_string("hello"))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
