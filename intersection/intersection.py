8d6b5604d62115458ab516c2ed1da309b1189fc4
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 15:20:26 +0000

    Solved 'intersection' (.py)

---
+++ intersection/intersection.py
@@ -0,0 +1,89 @@
+# Not efficient enough ! 
+def intersection(a, b):
+  a = set(a)
+  b = set(b)
+  a_min = min(a)
+  a_max = max(a)
+  b_min = min(b)
+  b_max = max(b)
+  a_min = a_max < b_max
+  inter = []
+  if a_max < b_min or a_min > b_max :
+    inter = []
+  elif a_min:
+    for element in a: 
+      if b_min <= element <= a_max and element in b:
+        inter.append(element)
+  else:
+    for element in a:
+      if a_min <= element <= b_max and element in b:
+        inter.append(element)  
+  return inter
+
+
+def intersection(lis_a, lis_b):
+  set_a = set(lis_a)
+  set_b = set(lis_b)
+  final = []
+  for ele in set_a:
+    if ele in set_b:
+      final.append(ele)
+  return final 
+
+
+def intersection_A(a, b):
+  set_a = set(a)
+  return [ item for item in b if item in set_a ]
+#Time: O(n+m)
+#Space: O(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
