1e0ad556bcdf576ad213328ec83f3d3574d21916
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-13 11:28:34 +0000

    Solved 'max value' (.py)

---
+++ max_value/max_value.py
@@ -0,0 +1,78 @@
+def max_value(nums):
+  temp_val = nums[0]
+  for i in nums:
+    if i > temp_val:
+      temp_val = i
+  return temp_val
+
+
+
+
+
+
+def max_value(lis):
+  val = lis[0]
+  for ele in lis:
+    if ele > val:
+      val = ele
+  return val
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
