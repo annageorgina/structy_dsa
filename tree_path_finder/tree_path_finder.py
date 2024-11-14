2603bc0f5c849208983ae7eea42a5e751221609b
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-14 20:34:13 +0000

    Solved 'tree path finder' (.py)

---
+++ tree_path_finder/tree_path_finder.py
@@ -0,0 +1,131 @@
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+# Recursivly think about what the return would be !! 
+
+def path_finder(root, target):
+  if root is None:
+    return None
+  if root.val == target:
+    return [ root.val ]
+    
+  left_path = path_finder(root.left, target)
+  if left_path is not None:
+    return [root.val, *left_path] # unpacking list = O(n)
+    
+  right_path = path_finder(root.right, target)
+  if right_path is not None:
+    return [root.val, *right_path]
+  
+  return None
+#Time O(n^2) append and recursion
+
+
+def path_finder(root, target):
+   result = _path_finder(root, target)
+   if result is None:
+     return None
+   else:
+    return result[::-1] #reverses a list in python
+
+def _path_finder(root, target):
+  if root is None:
+    return None
+  if root.val == target:
+    return [ root.val ]  
+  left_path = _path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path 
+    
+  right_path = _path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  
+  return None
+#Time O(n + n) = O
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+
+def tree_path_finder(root, target):
+  if root is None:
+    return None
+  if root.val == target:
+    return [root.val]
+  left_path = tree_path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  right_path = tree_path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  return None
+
+
+
+
+print(path_finder(a, 'e'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
