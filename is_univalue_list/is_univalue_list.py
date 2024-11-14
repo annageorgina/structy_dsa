a50c0e78daad6037f9fd83dba09fc456b1fcea1c
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-14 15:39:06 +0000

    Solved 'is univalue list' (.py)

---
+++ is_univalue_list/is_univalue_list.py
@@ -0,0 +1,90 @@
+class Node:
+   def __init__(self, val):
+     self.val = val
+     self.next = None
+
+
+a = Node(7)
+b = Node(7)
+c = Node(7)
+
+
+a.next = b
+b.next = c
+
+
+def is_univalue_list_i(head):
+  current = head
+  while current is not None:
+    if current.val != head.val:
+      return False
+    current = current.next
+  return True
+
+
+def is_univalue_list(head):
+  if head.next is None:
+    return True
+  if head.val != head.next.val:
+    return False
+  return is_univalue_list(head.next)
+  
+    
+    
+print(is_univalue_list(a))
+
+
+
+
+def is_univalue_list(head):
+  current = head
+  while current:
+    if current.val != head.val:
+      current = current.next
+    else:
+      return False
+  return True
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
