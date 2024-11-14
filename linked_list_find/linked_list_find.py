0fd5a13ff2c1aceb8a08afb70ceb6d84aa2e010a
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-14 10:41:47 +0000

    Solved 'linked list find' (.py)

--- linked_list_find/linked_list_find.py
+++ linked_list_find/linked_list_find.py
@@ -56,6 +56,20 @@ def linked_list_find(head, target):
   return False 
 
 
+#T O(n)
+#S O(1)
+
+
+
+
+def linked_list_find(head, target):
+  if head is None:
+    return False
+  if head.val == target:
+    return True
+  return linked_list_find(head.next, target)
+
+
 
 
 
