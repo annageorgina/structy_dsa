263faad65765203f7caa037ae50a342f91ddd870
annageorgina <82574593+annageorgina@users.noreply.github.com>
2024-11-14 20:43:06 +0000

    Solved 'tree path finder' (.py)

--- tree_path_finder/tree_path_finder.py
+++ tree_path_finder/tree_path_finder.py
@@ -1,3 +1,73 @@
+    return [ root.val ]  
+  left_path = _path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path 
+    
+  right_path = _path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  
+  return None
+#Time O(n + n) = O
+
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  return result[::-1]
+
+
+def _path_finder(root, target):
+  if root is None:
+    return None
+  if root.val == target:
+    return [root.val]
+  left_path = _path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  right_path = _path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
 a.left = b
 a.right = c
 b.left = d
@@ -37,48 +107,35 @@ def path_finder(root, target):
    else:
     return result[::-1] #reverses a list in python
 
+
 def _path_finder(root, target):
   if root is None:
     return None
   if root.val == target:
-    return [ root.val ]  
-  left_path = _path_finder(root.left, target)
-  if left_path is not None:
-    left_path.append(root.val)
-    return left_path 
-    
-  right_path = _path_finder(root.right, target)
-  if right_path is not None:
-    right_path.append(root.val)
     return right_path
   
   return None
 #Time O(n + n) = O
-a = Node("a")
-b = Node("b")
-c = Node("c")
-d = Node("d")
-e = Node("e")
-f = Node("f")
 
 
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.right = f
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
 
 
-def tree_path_finder(root, target):
+
+
+def _path_finder(root, target):
   if root is None:
     return None
   if root.val == target:
     return [root.val]
-  left_path = tree_path_finder(root.left, target)
+  left_path = _path_finder(root.left, target)
   if left_path is not None:
     left_path.append(root.val)
     return left_path
-  right_path = tree_path_finder(root.right, target)
+  right_path = _path_finder(root.right, target)
   if right_path is not None:
     right_path.append(root.val)
     return right_path
@@ -87,7 +144,6 @@ def tree_path_finder(root, target):
 
 
 
-print(path_finder(a, 'e'))
 
 
 
